cc871d6f3b9247edcabf30c7b6e654594e2ca87f
TimHam0304 <125191864+TimHam0304@users.noreply.github.com>
2025-04-14 20:13:05 +0200

    refactored home card

--- src/app/components/home/HomeCard.tsx
+++ src/app/components/home/HomeCard.tsx
@@ -1,10 +1,10 @@
 import Link, { LinkProps } from "next/link";
-import Image from "next/image";
+import { ReactNode } from "react";
 
 interface CardProps extends LinkProps {
   text: string;
   title: string;
-  imgSrc?: any;
+  children?: ReactNode;
 }
 /**
  * A homecard-link with a picture and text
@@ -12,22 +12,22 @@ interface CardProps extends LinkProps {
 export function HomeCardLinkPicure({
   text,
   title,
-  imgSrc,
+  children,
   ...props
 }: CardProps) {
   return (
     <Link
       {...props}
-      className={` bg-white dark:bg-neutral-800 rounded-2xl p-6 shadow-xl shadow-indigo-700/10 dark:shadow-inherit hover:shadow-none max-w-[28rem]hover:bg-neutral-100  dark:hover:bg-neutral-700 transition-all gray-outline-indigo-focus-outline`}
+      className={`flex flex-col w-[300px] h-[300px] bg-white dark:bg-neutral-800 rounded-2xl p-6 shadow-xl shadow-indigo-700/10 dark:shadow-inherit hover:shadow-none hover:bg-neutral-100  dark:hover:bg-neutral-700 transition-all gray-outline-indigo-focus-outline`}
     >
-      <div className="flex flex-col justify-center overflow-hidden w-full h-full">
-        {imgSrc && <Image width={300} height={300} src={imgSrc} alt=""></Image>}
-        <div className="pt-1">
-          <h2 className="font-semibold text-xl text-indigo-700 dark:text-inherit">
-            {title}
-          </h2>
-          <p className="text-sm font-normal dark:text-neutral-400">{text}</p>
-        </div>
+      <div className="flex justify-center items-center flex-1 w-full">
+        {children}
+      </div>
+      <div className="flex flex-col pt-1">
+        <h2 className="font-semibold text-xl text-indigo-700 dark:text-inherit">
+          {title}
+        </h2>
+        <p className="text-sm font-normal dark:text-neutral-400">{text}</p>
       </div>
     </Link>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import PokedexPNG from "@public/Pokedex.png";
 import { HomeCardLinkPicure } from "@components/home/HomeCard";
 import { HeroSection } from "@components/home/hero";
+import Image from "next/image";
+import { BulletList } from "./icons/list";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full h-auto">
+    <main className="flex flex-col w-full">
       <HeroSection />
       <section>
-        <div className="flex gap-6 flex-col px-6 items-center mx-auto max-w-7xl h-auto w-full">
+        <div className="flex gap-6 flex-col px-6 items-center mx-auto max-w-7xl w-full">
           <h2 className="font-extrabold text-indigo-700 dark:text-inherit antialiased text-2xl sm:text-4xl text-center">
             Explore our Content
           </h2>
@@ -16,25 +18,36 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex flex-wrap gap-4 py-10 px-6 justify-center items-center mx-auto max-w-7xl h-auto w-full">
+        <div className="flex flex-wrap gap-4 py-10 px-6 justify-center items-center mx-auto max-w-7xl w-full">
           <HomeCardLinkPicure
             href="/pokedex"
             title="Pokedex"
-            imgSrc={PokedexPNG}
             text="An infinite scroll pokedex with search function"
-          />
+          >
+            <Image
+              src={PokedexPNG}
+              className="h-44 w-44 object-contain"
+              alt=""
+            />
+          </HomeCardLinkPicure>
           <HomeCardLinkPicure
             href="/paginatedPokedex"
             title="Paginated Pokedex"
-            imgSrc={PokedexPNG}
             text="A paginated Pokedex with search function"
-          />
+          >
+            <Image
+              src={PokedexPNG}
+              className="h-44 w-44 object-contain"
+              alt=""
+            />
+          </HomeCardLinkPicure>
           <HomeCardLinkPicure
             href="/list"
             title="Pokemon list"
-            imgSrc={PokedexPNG}
             text="A simple list view of all existing Pokemon"
-          />
+          >
+            <BulletList className="size-44" />
+          </HomeCardLinkPicure>
         </div>
       </section>
     </main>
