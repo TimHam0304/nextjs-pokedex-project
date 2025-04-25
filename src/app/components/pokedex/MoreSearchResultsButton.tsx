bf9247aef42b284bfd0ee6a3bbbd05d8ddf91494
TimHam0304 <125191864+TimHam0304@users.noreply.github.com>
2025-04-25 17:53:01 +0200

    style update

--- src/app/components/pokedex/MoreSearchResultsButton.tsx
+++ src/app/components/pokedex/MoreSearchResultsButton.tsx
@@ -7,17 +7,11 @@ export function ShowMoreResultsButton({
   onClick,
   disabled,
 }: ShowMoreResultsButtonProps) {
-  const baseStyle =
-    "bg-indigo-600 hover:bg-indigo-800 dark:bg-neutral-800 dark:hover:bg-neutral-700 shadow-md text-white";
-  const disabledStyle = "bg-transparent cursor-not-allowed";
-
   return (
     <button
       aria-disabled={disabled}
       onClick={() => onClick()}
-      className={`${
-        disabled ? disabledStyle : baseStyle
-      } px-8 py-3 rounded-md font-medium indigo-focus-outline focus-visible:outline-offset-2`}
+      className={`bg-indigo-600 hover:bg-indigo-800 dark:bg-neutral-800 dark:hover:bg-neutral-700 shadow-md text-white aria-disabled:bg-transparent aria-disabled:cursor-not-allowed aria-disabled:hover:bg-transparent aria-disabled:text-inherit aria-disabled:shadow-none px-8 py-3 rounded-md font-medium indigo-focus-outline focus-visible:outline-offset-2`}
     >
       {disabled ? "All results displayed" : "Show more results"}
     </button>
