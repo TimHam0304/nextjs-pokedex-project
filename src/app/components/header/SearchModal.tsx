b22a93f90e2db09fc24fcbd9a4f811474cbd8a61
TimHam0304 <125191864+TimHam0304@users.noreply.github.com>
2025-05-02 18:35:49 +0200

    added search suggestions

--- src/app/components/header/SearchModal.tsx
+++ src/app/components/header/SearchModal.tsx
@@ -5,6 +5,7 @@ import { Fragment } from "react";
 import { ArrowInBoxTopRight } from "@icons/arrowInBox";
 import { useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@/app/icons/MagnifyingGlass";
+import Link from "next/link";
 
 const searchPaths = [
   {
@@ -21,8 +22,7 @@ interface SearchModalProps {
   pokemonList: string[] | null;
 }
 
-//TODO add search recommendations based on input
-//TODO add search button
+//TODO make search suggestions accessible with keyboard up/down instead of tab
 export function SearchModal({ pokemonList }: SearchModalProps) {
   const dialogRef = useRef<HTMLDialogElement | null>(null);
   const [activeRoute, setActiveRoute] = useState<string | null>(
@@ -31,9 +31,11 @@ export function SearchModal({ pokemonList }: SearchModalProps) {
   const [query, setQuery] = useState("");
   const { push } = useRouter();
 
-  function onButtonClick(route: string) {
-    setActiveRoute(route);
-  }
+  const filteredQueryList = query
+    ? pokemonList
+        ?.filter((name) => name.toLowerCase().includes(query.toLowerCase()))
+        .slice(0, 10)
+    : [];
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -90,23 +92,30 @@ export function SearchModal({ pokemonList }: SearchModalProps) {
           role="search"
           className="flex flex-col items-center gap-4 px-8 pb-8"
         >
-          <div className="flex gap-2 w-full items-center text-xs">
-            {searchPaths.map((searchPaths) => (
-              <button
-                type="button"
-                key={searchPaths.label}
-                className={`rounded-md p-1 indigo-focus-outline outline-offset-2 ${
-                  activeRoute === searchPaths.route
-                    ? "bg-indigo-600 text-white"
-                    : "dark:bg-neutral-700 bg-neutral-200"
-                }`}
-                onClick={() => onButtonClick(searchPaths.route)}
-              >
-                {searchPaths.label}
-              </button>
-            ))}
-          </div>
-
+          <fieldset className="flex w-full">
+            <legend className="sr-only">Pokedex type</legend>
+            <div className="flex gap-2 items-center text-xs has-[:focus-visible]:outline outline-2 outline-indigo-700 dark:outline-indigo-500 outline-offset-8 rounded-sm">
+              {searchPaths.map((searchPaths) => (
+                <div key={searchPaths.label}>
+                  <input
+                    className="peer sr-only"
+                    type="radio"
+                    id={searchPaths.label}
+                    name="set1"
+                    value={searchPaths.route}
+                    checked={activeRoute === searchPaths.route}
+                    onChange={(e) => setActiveRoute(e.target.value)}
+                  />
+                  <label
+                    htmlFor={searchPaths.label}
+                    className="inline-block rounded-md p-1 text-xs dark:bg-neutral-700 bg-neutral-200 peer-checked:bg-indigo-600 peer-checked:text-white cursor-pointer"
+                  >
+                    {searchPaths.label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </fieldset>
           <input
             className="w-full bg-white dark:bg-neutral-800 placeholder:text-neutral-400 dark:placeholder:text-neutral-300/70 indigo-focus-outline rounded-lg p-2 shadow-md shadow-indigo-700/10 dark:shadow-inherit border border-neutral-300 dark:border-neutral-700 [&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none"
             placeholder="Search..."
@@ -114,6 +123,29 @@ export function SearchModal({ pokemonList }: SearchModalProps) {
             onChange={(e) => setQuery(e.target.value)}
             value={query}
           ></input>
+          {filteredQueryList && (
+            <div
+              aria-hidden
+              className="w-full border-t-2 border-indigo-500"
+            ></div>
+          )}
+          {filteredQueryList && (
+            <div
+              aria-label="Search suggestions matching the query"
+              className="flex flex-col w-full h-60 max-h-60 overflow-scroll rounded-md indigo-focus-outline"
+            >
+              {filteredQueryList.map((entry) => (
+                <Link
+                  href={`${activeRoute}?search=${entry}`}
+                  onClick={closeModal}
+                  className="flex items-center pl-2 min-h-10 hover:bg-neutral-700 rounded-md focus-visible:bg-neutral-700 outline-none"
+                  key={entry}
+                >
+                  {entry[0].toUpperCase() + entry.slice(1)}
+                </Link>
+              ))}
+            </div>
+          )}
         </form>
       </dialog>
       <button
