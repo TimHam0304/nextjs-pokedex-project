6fd5682f403199cc0831e0b6d120b491bffb0e01
TimHam0304 <125191864+TimHam0304@users.noreply.github.com>
2025-05-22 18:27:55 +0200

    added tmp content to page

--- src/app/(pages)/ability/[slug]/page.tsx
+++ src/app/(pages)/ability/[slug]/page.tsx
@@ -1,12 +1,23 @@
 import { getAbility } from "@actions/AbilityActions";
 import { FetchError } from "@components/error/fetchErrorPage";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { AbilityPokemon } from "@models/Pokemon/ability";
 
 interface AbilityPageProps {
   params: Promise<{ slug: string }>;
 }
 
-//TODO
+export async function generateMetadata(
+  props: AbilityPageProps
+): Promise<Metadata> {
+  const params = await props.params;
+  return {
+    title: `Ability - ${params.slug}`,
+  };
+}
+
+//TODO finish styling this page
 export default async function AbilityPage(props: AbilityPageProps) {
   const params = await props.params;
   const { ability, status } = await getAbility(params.slug);
@@ -19,14 +30,25 @@ export default async function AbilityPage(props: AbilityPageProps) {
     }
   }
 
+  const englishEffect = ability.effect_entries.find(
+    (entry) => entry.language.name === "en"
+  );
+
   return (
     <main className="flex flex-col gap-6 py-20 px-6 mx-auto max-w-7xl h-auto w-full items-center">
       <h1 className="font-bold text-3xl text-center text-indigo-700 dark:text-inherit">
         {ability.name}
       </h1>
-      <h2 className="text-sm text-center">Flavor text</h2>
-      <section className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 w-full h-auto">
-        more text
+      {englishEffect && (
+        <h2 className="text-sm text-center">{englishEffect.effect}</h2>
+      )}
+      <section className="flex flex-col w-full justify-center items-center gap-2">
+        <div>{englishEffect?.short_effect}</div>
+        <div>first appearance: {ability.generation.name.toUpperCase()}</div>
+        <h3>List of pokemons with this ability:</h3>
+        {ability.pokemon.map((entry: AbilityPokemon) => (
+          <div key={entry.pokemon.name}>{entry.pokemon.name}</div>
+        ))}
       </section>
     </main>
   );
