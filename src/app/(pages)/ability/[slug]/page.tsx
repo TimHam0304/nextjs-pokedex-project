d55e26418b4601c5dc6fabe731ff098b521c0c8d
TimHam0304 <125191864+TimHam0304@users.noreply.github.com>
2025-05-25 03:02:26 +0200

    updated design

--- src/app/(pages)/ability/[slug]/page.tsx
+++ src/app/(pages)/ability/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { FetchError } from "@components/error/fetchErrorPage";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { AbilityPokemon } from "@models/Pokemon/ability";
+import Link from "next/link";
 
 interface AbilityPageProps {
   params: Promise<{ slug: string }>;
@@ -17,7 +18,7 @@ export async function generateMetadata(
   };
 }
 
-//TODO finish styling this page
+//TODO improve design of page
 export default async function AbilityPage(props: AbilityPageProps) {
   const params = await props.params;
   const { ability, status } = await getAbility(params.slug);
@@ -37,18 +38,47 @@ export default async function AbilityPage(props: AbilityPageProps) {
   return (
     <main className="flex flex-col gap-6 py-20 px-6 mx-auto max-w-7xl h-auto w-full items-center">
       <h1 className="font-bold text-3xl text-center text-indigo-700 dark:text-inherit">
-        {ability.name}
+        {ability.name[0].toUpperCase() + ability.name.slice(1)}
       </h1>
-      {englishEffect && (
-        <h2 className="text-sm text-center">{englishEffect.effect}</h2>
-      )}
-      <section className="flex flex-col w-full justify-center items-center gap-2">
-        <div>{englishEffect?.short_effect}</div>
-        <div>first appearance: {ability.generation.name.toUpperCase()}</div>
-        <h3>List of pokemons with this ability:</h3>
-        {ability.pokemon.map((entry: AbilityPokemon) => (
-          <div key={entry.pokemon.name}>{entry.pokemon.name}</div>
-        ))}
+      <section className="flex flex-col text-center gap-2">
+        <h2 className="text-xl font-bold">Effect</h2>
+        {englishEffect && (
+          <p className="text-sm text-center">{englishEffect.effect}</p>
+        )}
+      </section>
+      <section className="flex flex-col text-center gap-2">
+        <h2 className="text-xl font-bold">Short Effect</h2>
+        {englishEffect && (
+          <p className="text-sm text-center">{englishEffect.short_effect}</p>
+        )}
+      </section>
+      <section className="flex flex-col text-center gap-2">
+        <h2 className="text-xl font-bold">First Appearance</h2>
+
+        <p className="text-sm text-center">
+          {ability.generation.name.toUpperCase()}
+        </p>
+      </section>
+      <section className="flex flex-col w-full text-center gap-2">
+        <h2 className="text-xl font-bold">
+          List of pokemons with this ability
+        </h2>
+        <p className="text-sm text-center">
+          A * next to the name means the ability is hidden
+        </p>
+        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 w-full">
+          {ability.pokemon.map((entry: AbilityPokemon) => (
+            <Link
+              href={`/pokemon/${entry.pokemon.name}`}
+              className="text-center hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-md py-1 indigo-focus-outline"
+              key={entry.pokemon.name}
+            >
+              {entry.pokemon.name[0].toUpperCase() +
+                entry.pokemon.name.slice(1)}
+              {entry.is_hidden ? " *" : ""}
+            </Link>
+          ))}
+        </div>
       </section>
     </main>
   );
