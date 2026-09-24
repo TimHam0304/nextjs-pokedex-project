ef52984ed38a0c6f0e519170d7e1079c4340360c
TimH <125191864+TimHam0304@users.noreply.github.com>
2026-09-24 13:26:44 +0200

    changed component name

--- src/app/(pages)/favorites/page.tsx
+++ src/app/(pages)/favorites/page.tsx
@@ -2,7 +2,7 @@ import { FavoritesGrid } from "@components/favorites/FavoritesGrid";
 import { Suspense } from "react";
 import { PokedexComponentSkeleton } from "@/app/components/misc/PokedexLoadingSkeleton";
 
-export default async function ListPage() {
+export default async function FavoritesPage() {
   return (
     <main className="flex flex-col gap-6 py-20 px-6 mx-auto max-w-7xl w-full items-center">
       <h1 className="font-bold text-3xl text-center text-indigo-700 dark:text-indigo-300">
