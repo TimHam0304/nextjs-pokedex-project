b566699c49eb1ac518ba049cbd7446cc87c8e66a
TimH <125191864+TimHam0304@users.noreply.github.com>
2026-09-24 13:26:15 +0200

    removed todo

--- src/app/components/misc/LikeButton.tsx
+++ src/app/components/misc/LikeButton.tsx
@@ -6,7 +6,6 @@ import { LinkIcon } from "@icons/link";
 import { BASE_URL } from "@constants";
 
 //TODO add this to the detailed card
-//TODO add dedicated favorites page
 export function LikeAndShareBox({ name }: { name: string }) {
   const { toggleFavorite, isFavorite } = useFavoritesStore((state) => state);
   const isFav = isFavorite(name);
